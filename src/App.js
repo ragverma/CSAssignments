b8556ca975cfc414b2a5d7e579286f49a00de1e5
ragverma <rv00027@mix.wvu.edu>
2024-02-27 17:03:01 -0500

    NavBar and Cards

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import './Navbar.js';
+import './Card.js';
+
 
 function App() {
   return (
     <div className="App">
-       <h1> CS 230L </h1>
+      <Navbar />
+      <h1> CS 230L </h1>
       <h2>Section - 02</h2>
       <p>WVU ID: 800434127</p>
       <p>Hi I am Ragini</p>
+      <div className = "card-container">
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   );
 }
---
+++ src/Card.js
@@ -0,0 +1,13 @@
+// Card.js
+import React from 'react';
+
+const Card = () => {
+  return (
+    <div className="card">
+      <h3>Card Title</h3>
+      <p>This is a card component.</p>
+    </div>
+  );
+};
+
+export default Card;
